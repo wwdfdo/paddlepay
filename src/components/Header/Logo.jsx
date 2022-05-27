683d35f27931eea3b0cda21fcf429507b8deaa02
wwdfdo <wwdfdo@gmail.com>
2022-05-27 19:39:38 +0530

    added updated logo

--- src/components/Header/Logo.jsx
+++ src/components/Header/Logo.jsx
@@ -1,9 +1,9 @@
 import React from "react";
-import newlogo from "../../images/newlogo.png";
+import newlogo from "../../images/updatelogo.png";
 
 const Logo = () => {
   return (
-    <div className="w-[80px]">
+    <div className="w-[65px]">
       {/* <img src={logo} alt="lOGO" /> */}
       <img src={newlogo} alt="logoThree" />
       {/* <img src={logoTwo} alt="lOGOTWO" /> */}
