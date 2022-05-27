de4f16ed1c36f31e3051d271004cc8aff30fc466
wwdfdo <wwdfdo@gmail.com>
2022-05-27 17:42:09 +0530

    gradient seen

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -5,7 +5,7 @@ import SectionTwo from "./SectionTwo/SectionTwo";
 
 const Body = () => {
   return (
-    <div className="w-full bg-gradient-to-r from-[#06122e]">
+    <div className="w-full bg-gradient-to-r from-[#06122e] to-[#0b017a]">
       <Header />
       <SectionTwo />
       <SectionThree />
