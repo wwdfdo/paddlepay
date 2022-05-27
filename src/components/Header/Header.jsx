f9fe1afc0edaf2d7dc7509cc7cdb739d968caef3
wwdfdo <wwdfdo@gmail.com>
2022-05-27 12:31:40 +0530

    added colors section one

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -9,25 +9,13 @@ const Header = () => {
     <div className="w-4/5 flex justify-between mx-auto">
       <Logo />
       <div className="flex gap-8 items-center text-base font-semibold">
-        <div>
-          <a href="#">Home</a>
-        </div>
-        <div>
-          <a href="#">About</a>
-        </div>
-        <div>
-          <a href="#">Features</a>
-        </div>
-        <div>
-          <a href="#">Dapp</a>
-        </div>
-        <div>
-          <a href="#">Team</a>
-        </div>
-        <div>
-          <a href="#">Roadmap</a>
-        </div>
-        <div className="bg-[#8A6FDF] py-3 px-5 rounded-md">NFT</div>
+        <div>Home</div>
+        <div>About</div>
+        <div>Features</div>
+        <div>Dapp</div>
+        <div>Team</div>
+        <div>Roadmap</div>
+        <div className="bg-[#5b7fd7] py-3 px-5 rounded-md">NFT</div>
       </div>
     </div>
   );
--- src/components/SectionTwo/SectionTwo.jsx
+++ src/components/SectionTwo/SectionTwo.jsx
@@ -1,17 +1,17 @@
 import React from "react";
-import ridebike from "../../images/ridebike.gif";
+import ridebike from "../../images/paddlepay-bike.jpeg";
 import Aboutinfoshape from "../../images/about-info-shape.png";
 
 const SectionTwo = () => {
   return (
-    <div className="flex w-5/6 mx-auto gap-12 items-start">
-      <div className="w-1/2">
+    <div className="flex w-5/6 mx-auto gap-12 items-center">
+      <div className="w-1/2 mt-20">
         <img src={ridebike} alt="" />
       </div>
       <div className="mt-16">
         <div className="mb-10">
           <h2 className="text-[42px] leading-loose font-bold">
-            Social-Fi & Game-Fi <span className="text-[#7977ee]">DAPP</span>
+            Social-Fi & Game-Fi <span className="text-[#2a77d7]">DAPP</span>
           </h2>
         </div>
         <div className="border-l-4 border-[#8A6FDF] text-[24px] font-bold relative">
@@ -21,27 +21,35 @@ const SectionTwo = () => {
           <h2 className="ml-10 mt-10 py-8">
             Revolutionary Ride-To-Earn
             <br />
-            <span className="text-[#7977ee]">Since 2022</span>
+            <span className="text-[#2a77d7]">Since 2022</span>
           </h2>
         </div>
         <div className="border-l-4 border-[#8A6FDF] mt-10 py-6">
           <div className="flex ml-10 mb-8 items-center">
             <div className="w-1/2">
-              <p className="text-[30px] leading[36px] font-bold">3,000+</p>
+              <p className="text-[30px] leading[36px] font-bold text-[#fcbb29]">
+                3,000+
+              </p>
               <p>Active Users</p>
             </div>
             <div className="w-1/2">
-              <p className=" text-[30px] leading[36px] font-bold">6,000+</p>
+              <p className=" text-[30px] leading[36px] font-bold text-[#ac72c9]">
+                6,000+
+              </p>
               <p>Tasks Completed</p>
             </div>
           </div>
           <div className="flex ml-10">
             <div className="w-1/2">
-              <p className=" text-[30px] leading[36px] font-bold">80+</p>
+              <p className=" text-[30px] leading[36px] font-bold text-[#83d840]">
+                80+
+              </p>
               <p>Partner</p>
             </div>
             <div className="w-1/2">
-              <p className=" text-[30px] leading[36px] font-bold">4,000+</p>
+              <p className=" text-[30px] leading[36px] font-bold text-[#2a77d7]">
+                4,000+
+              </p>
               <p>Positive Reviews</p>
             </div>
           </div>
