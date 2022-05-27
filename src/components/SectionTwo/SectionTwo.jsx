fe13414c2bc5539678f97585845e6d4596329abd
wwdfdo <wwdfdo@gmail.com>
2022-05-27 20:31:02 +0530

    change sectiontwo

--- src/components/SectionTwo/SectionTwo.jsx
+++ src/components/SectionTwo/SectionTwo.jsx
@@ -5,17 +5,14 @@ import Aboutinfoshape from "../../images/about-info-shape.png";
 const SectionTwo = () => {
   return (
     <div className="flex w-5/6 mx-auto gap-12 items-center py-32">
-      <div className="w-1/2 mt-20">
-        <img src={ridebike} alt="" />
-      </div>
       <div className="mt-16">
         <div className="mb-10">
           <h2 className="text-[42px] leading-loose font-bold text-white">
             Social-Fi & Game-Fi <span className="text-[#2a77d7]">DAPP</span>
           </h2>
         </div>
-        <div className="border-l-4 border-[#8A6FDF] text-[24px] font-bold relative">
-          <div className="absolute right-0">
+        <div className="border-r-4 border-[#8A6FDF] text-[24px] font-bold relative pl-20">
+          <div className="absolute left-0">
             <img src={Aboutinfoshape} alt="" />
           </div>
           <h2 className="ml-10 mt-10 py-8 text-white">
@@ -24,7 +21,7 @@ const SectionTwo = () => {
             <span className="text-[#2a77d7]">Since 2022</span>
           </h2>
         </div>
-        <div className="border-l-4 border-[#8A6FDF] mt-10 py-6">
+        <div className="border-r-4 border-[#8A6FDF] mt-10 py-6 pl-20">
           <div className="flex ml-10 mb-8 items-center">
             <div className="w-1/2">
               <p className="text-[30px] leading[36px] font-bold text-[#9cdd69]">
@@ -54,6 +51,9 @@ const SectionTwo = () => {
             </div>
           </div>
         </div>
+      </div>
+      <div className="w-1/2 mt-20">
+        <img src={ridebike} alt="" />
       </div>
     </div>
   );
