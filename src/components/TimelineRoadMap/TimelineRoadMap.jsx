4f01a2321157046a8bb003699aaf4267e6337ee9
wwdfdo <wwdfdo@gmail.com>
2022-06-01 12:13:25 +0530

    add title to roadmap

--- src/components/TimelineRoadMap/TimelineRoadMap.jsx
+++ src/components/TimelineRoadMap/TimelineRoadMap.jsx
@@ -3,7 +3,8 @@ import timelinebg from "../../images/timelinebg.png";
 
 const TimelineRoadMap = () => {
   return (
-    <div id="Roadmap" className="py-10">
+    <div id="Roadmap" className="pb-10 pt-24">
+      <h2 className="text-white font-bold text-6xl text-center">Road Map</h2>
       <div className="bg-roadmapbg bg-cover bg-no-repeat mx-auto py-6 my-10">
         <img className="w-5/6 mx-auto" src={timelinebg} alt="" />
       </div>
